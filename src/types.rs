7d9c6a867eaa34dcfed15461757b2a9eda7f27e2
negativefix <alper.buluc@gmail.com>
2022-07-04 14:12:56 +0300

    update: make type and type fields public

--- src/types.rs
+++ src/types.rs
@@ -12,80 +12,75 @@ pub struct ExchangeInfo {
 
 #[derive(Debug)]
 struct Symbol {
-    id: u64,
-    name: String,
-    name_normalized: String,
-    status: String,
-    numerator: String,
-    denominator: String,
-    numeral_scale: u64,
-    denominator_scale: u64,
-    has_fraction: bool,
-    filters: Vec<SymbolFilter>,
-    order_methods: Vec<String>,
-    display_format: String,
-    commisson_from_numerator: bool,
-    order: u64,
-    price_rounding: bool,
-    is_new: bool,
-    market_price_warning_threshold_percentage: f64,
-    maximum_order_amount: Option<f64>,
-    maximum_limit_order_price: f64,
-    minimum_limit_order_price: f64,
+    pub id: u64,
+    pub name: String,
+    pub name_normalized: String,
+    pub status: String,
+    pub numerator: String,
+    pub denominator: String,
+    pub numeral_scale: u64,
+    pub denominator_scale: u64,
+    pub has_fraction: bool,
+    pub filters: Vec<SymbolFilter>,
+    pub order_methods: Vec<String>,
+    pub display_format: String,
+    pub commisson_from_numerator: bool,
+    pub order: u64,
+    pub price_rounding: bool,
+    pub is_new: bool,
+    pub market_price_warning_threshold_percentage: f64,
+    pub maximum_order_amount: Option<f64>,
+    pub maximum_limit_order_price: f64,
+    pub minimum_limit_order_price: f64,
 }
 
 #[derive(Debug)]
 struct SymbolFilter {
-    filter_type: String,
-    min_price: f64,
-    max_price: f64,
-    tick_size: u64,
-    min_exchange_value: f64,
-    min_amount: Option<f64>,
-    max_amount: Option<f64>,
+    pub filter_type: String,
+    pub min_price: f64,
+    pub max_price: f64,
+    pub tick_size: u64,
+    pub min_exchange_value: f64,
+    pub min_amount: Option<f64>,
+    pub max_amount: Option<f64>,
 }
 
 #[derive(Debug)]
 struct Address {
-    min_len: Option<u64>,
-    max_len: Option<u64>,
+    pub min_len: Option<u64>,
+    pub max_len: Option<u64>,
 }
 
 #[derive(Debug)]
-struct Tag {
-    enable: bool,
-    name: Option<String>,
-    min_len: Option<f64>,
-    max_len: Option<f64>,
+pub struct Tag {
+    pub enable: bool,
+    pub name: Option<String>,
+    pub min_len: Option<f64>,
+    pub max_len: Option<f64>,
 }
 
 #[derive(Debug)]
 struct Currency {
-    id: u64,
-    symbol: String,
-    min_withdrawal: f64,
-    min_deposit: f64,
-    precision: u64,
-    address: Address,
-    currency_type: String,
-    tag: Tag,
-    color: String,
-    name: String,
-    is_address_renewable: bool,
-    get_auto_address_disabled: bool,
-    is_partial_withdrawal_enabled: bool,
-    is_new: bool,
+    pub id: u64,
+    pub symbol: String,
+    pub min_withdrawal: f64,
+    pub min_deposit: f64,
+    pub precision: u64,
+    pub address: Address,
+    pub currency_type: String,
+    pub tag: Tag,
+    pub color: String,
+    pub name: String,
+    pub is_address_renewable: bool,
+    pub get_auto_address_disabled: bool,
+    pub is_partial_withdrawal_enabled: bool,
+    pub is_new: bool,
 }
 
 #[derive(Debug)]
-struct CurrencyOperationStatus {
-    currency_symbol: String,
-    withdraw_disabled: bool,
-    deposit_disabled: bool,
+pub struct CurrencyOperationStatus {
+    pub currency_symbol: String,
+    pub withdraw_disabled: bool,
+    pub deposit_disabled: bool,
 }
-
-
-
-
-
 
