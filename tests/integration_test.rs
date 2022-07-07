d609a2a7772eb514e47f39e0542b0b3da5f53f9b
negativefix <alper.buluc@gmail.com>
2022-07-07 19:53:34 +0300

    add: default test for kline method

--- tests/integration_test.rs
+++ tests/integration_test.rs
@@ -1,36 +1,43 @@
-use btcturk_rust::{BTCTRResult, Api, BASE_URL};
+use btcturk_rust::{BTCTRResult, Api, BASE_API_URL, GRAPH_API_URL};
 
 #[tokio::test]
 async fn exchange_info() -> BTCTRResult<()> {
-    let api = Api::new(BASE_URL, None);
+    let api = Api::new(BASE_API_URL, None);
     let _json = api.exchange_info().await?;
     Ok(())
 }
 
 #[tokio::test]
 async fn ticker_pair() -> BTCTRResult<()> {
-    let api = Api::new(BASE_URL, None);
+    let api = Api::new(BASE_API_URL, None);
     let _json = api.pair(None).await?;
     Ok(())
 }
 
 #[tokio::test]
 async fn ticker_currency() -> BTCTRResult<()> {
-    let api = Api::new(BASE_URL, None);
+    let api = Api::new(BASE_API_URL, None);
     let _json = api.currency("BTCTRY").await?;
     Ok(())
 }
 
 #[tokio::test]
 async fn order_book() -> BTCTRResult<()> {
-    let api = Api::new(BASE_URL, None);
+    let api = Api::new(BASE_API_URL, None);
     let _json = api.order_book("BTCTRY", None).await?;
     Ok(())
 }
 
 #[tokio::test]
 async fn trade() -> BTCTRResult<()> {
-    let api = Api::new(BASE_URL, None);
+    let api = Api::new(BASE_API_URL, None);
     let _json = api.trades("BTCTRY", None).await?;
     Ok(())
 }
+
+#[tokio::test]
+async fn kline_default() -> BTCTRResult<()> {
+    let api = Api::new(BASE_API_URL, None);
+    let _json = api.kline_data("BTCTRY", 80, 1602925320, 1603152000).await?;
+    Ok(())
+}  
