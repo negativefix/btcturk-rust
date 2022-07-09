ab9b31f3fcc729e9af03999c371dde9decc3980d
negativefix <alper.buluc@gmail.com>
2022-07-09 11:43:09 +0300

    update: make methods consistent

--- src/lib.rs
+++ src/lib.rs
@@ -14,7 +14,7 @@ mod errors;
 
 pub use errors::BTCTRResult;
 use reqwest::{Response, Url};
-use types::{
+pub use types::{
     ExchangeInfo, 
     Pair, 
     OrderBook, 
@@ -26,6 +26,14 @@ use types::{
     TradesParams, 
     OrderBookParams
 };
+
+
+fn create_public_endpoint_url(path: &str, graph: bool) -> Result<reqwest::Url, url::ParseError> {
+    let base_url = if graph { GRAPH_API_URL } else { BASE_API_URL };
+    let mut url = Url::parse(base_url)?;
+    url.set_path(path);
+    Ok(url)
+}
 
 // Config for authenticated requests
 // better name would be AuthConfig
@@ -61,77 +69,67 @@ impl Api {
     
     // Public api endpoints
     pub async fn exchange_info(&self) -> BTCTRResult<ExchangeInfo> {
-        let response = self.fetch("/api/v2/server/exchangeinfo").await?;
-        let json: ExchangeInfo = response.json().await?;
-        Ok(json)
+        let url = create_public_endpoint_url("/api/v2/server/exchangeinfo", false)?;
+        let data = reqwest::get(url.as_str()).await?.json().await?;
+        Ok(data)
     }
 
     // no need to pass self since it wont be used within the function
     // TODO - move this into a module
     pub async fn pair(&self, pair_symbol: Option<&str>) -> BTCTRResult<Pair> {
-        let endpoint = format!("{}{}", self.base_url, "/api/v2/ticker");
-        let url = match pair_symbol {
-            Some(pair) => Url::parse_with_params(&endpoint, &[("pairSymbol", pair)])?,
-            None => Url::parse(&endpoint)?,
-        };
-        let json = reqwest::get(url.as_str()).await?.json().await?;
-        Ok(json)
+        let mut url = create_public_endpoint_url("/api/v2/ticker", false)?;
+        if let Some(pair) = pair_symbol {
+            url
+                .query_pairs_mut()
+                .append_pair("pairSymbol", pair);
+        }
+        let data = reqwest::get(url.as_str()).await?.json().await?;
+        Ok(data)
     }
 
     pub async fn currency(&self, symbol: &str) -> BTCTRResult<Pair>{
-        let endpoint = format!("{}{}", self.base_url, "/api/v2/ticker/currency");
-        let url = Url::parse_with_params(&endpoint, &[("symbol", symbol)])?;
-        let json = reqwest::get(url.as_str()).await?.json().await?;
-        Ok(json)
-    }
-    
-    pub async fn order_book(&self, params: OrderBookParams) -> BTCTRResult<OrderBook> {
-        let endpoint = format!("{}{}", self.base_url, "/api/v2/orderbook");
-        let pair = &[("pairSymbol", pair_symbol)];
-        let mut url = Url::parse_with_params(&endpoint, pair)?;
-        if let Some(l) = limit {
-            url.query_pairs_mut().append_pair("limit", &l.to_string());
-        }
-        let json = reqwest::get(url.as_str()).await?.json().await?;
-        Ok(json)
-    }
-
-    pub async fn trades(&self, params: TradesParams) -> BTCTRResult<Trade> {
-        let endpoint = format!("{}{}", self.base_url, "/api/v2/trades");
-        let pair = &[("pairSymbol", pair_symbol)];
-        let mut url = Url::parse_with_params(&endpoint, pair)?;
-        if let Some(l) = last {
-            url.query_pairs_mut().append_pair("last", &l.to_string());
-        }
-        let json = reqwest::get(url.as_str()).await?.json().await?;
-        Ok(json)
-    }
-
-    pub async fn ohlc_data(&self, params: OhlcParams) -> BTCTRResult<Ohlc> {
-        let mut url = Url::parse(GRAPH_API_URL)?;
-        url.set_path("/v1/klines/history");
-        url.query_pairs_mut().append_pair("symbol", pair_symbol);
-        if let Some(f) = from {
-            url.query_pairs_mut().append_pair("from", &f.to_string());
-        }
-        if let Some(t) = to {
-            url.query_pairs_mut().append_pair("to", &t.to_string());
-        }
-        let json = reqwest::get(url.as_str()).await?.json().await?;
-        Ok(json)
-    }
-
-    pub async fn kline_data(&self, params: KlineParams) -> BTCTRResult<Kline>{
-        let mut url = Url::parse(GRAPH_API_URL)?;
-        url.set_path("/v1/klines/history");
+        let mut url = create_public_endpoint_url("/api/v2/ticker/currency", false)?;
         url
             .query_pairs_mut()
-            .append_pair("symbol", pair_symbol)
-            .append_pair("to", &to.to_string())
-            .append_pair("from", &from.to_string())
-            .append_pair("resolution", &resolution.to_string());
-        let json = reqwest::get(url.as_str()).await?.json().await?;
-        Ok(json)
+            .append_pair("symbol", symbol);
+        let data = reqwest::get(url.as_str()).await?.json().await?;
+        Ok(data)
+    }
+    
+    pub async fn order_book(&self, pair_symbol: &str, limit: Option<u32>) -> BTCTRResult<OrderBook> {
+        let mut url = create_public_endpoint_url("/api/v2/orderbook", false)?;
+        let params = OrderBookParams { pair_symbol, limit };
+        let url_params = serde_url_params::to_string(&params)?;
+        url.set_query(Some(&url_params));
+        let data = reqwest::get(url.as_str()).await?.json().await?;
+        Ok(data)
+    }
+
+    pub async fn trades(&self, pair_symbol: &str, last: Option<u32>) -> BTCTRResult<Trade> {
+        let mut url = create_public_endpoint_url("/api/v2/trades", false)?;
+        let params = TradesParams { pair_symbol, last };
+        let url_params = serde_url_params::to_string(&params)?;
+        url.set_query(Some(&url_params));
+        let data = reqwest::get(url.as_str()).await?.json().await?;
+        Ok(data)
+    }
+
+    pub async fn ohlc_data(&self, pair_symbol: &str, from: Option<u64>, to: Option<u64>) -> BTCTRResult<Ohlc> {
+        let mut url = create_public_endpoint_url("/v1/ohlcs", true)?;
+        let params = OhlcParams { pair_symbol, from, to };
+        let url_params = serde_url_params::to_string(&params)?;
+        url.set_query(Some(&url_params));
+        let data = reqwest::get(url.as_str()).await?.json().await?;
+        Ok(data)
+    }
+
+    pub async fn kline_data(&self, pair_symbol: &str, from: u64, to: u64, resolution: u64) -> BTCTRResult<Kline>{
+        let mut url = create_public_endpoint_url("/v1/klines/history", true)?;
+        let params = KlineParams { pair_symbol, from, to, resolution };
+        let query_params = serde_url_params::to_string(&params)?;
+        url.set_query(Some(&query_params));
+        let data = reqwest::get(url.as_str()).await?.json().await?;
+        Ok(data)
     }
 
     // Private api endpoints
@@ -152,12 +150,6 @@ impl Api {
         pair_symbol: &str
         ) {}
 
-    pub async fn fetch(&self, endpoint: &str) -> BTCTRResult<Response> {
-        let url = format!("{}{}", self.base_url, endpoint);
-        let res = reqwest::get(&url).await?;
-        println!("{}", url);
-        Ok(res)
-    }
 }
 
 
